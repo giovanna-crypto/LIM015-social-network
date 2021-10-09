eb561648e8a932733820c09fa5cd6e64436c535b
giovanna-crypto <giovannalopez826@gmail.com>
2021-10-08 17:17:41 -1100

    aplicando funcion auth google

--- src/main.js
+++ src/main.js
@@ -1,27 +1,26 @@
-// Este es el punto de entrada de tu aplicacion
+import { login, logout } from './firebase/firebase-fn.js';
 
-// Initialize Firebase
+const buttonLogin = document.querySelector('#btn-Google');
+const buttonLogout = document.querySelector('#btn-OutLog ');
+// let para mantener la variable del usuario logueado
+let currentUser;
+// metodo para saber si el usuario esta logueado o no
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    currentUser = user;
+    console.log('Usuario logueado', currentUser.displayName);
+  }
+});
 
-/* import { myFunction } from './lib/index.js';
+buttonLogin.addEventListener('click', async (e) => {
+  try {
+    currentUser = await login();
+  } catch (error) {
+    console.log(e);
+  }
+});
 
-myFunction(); */
-
-// Google Login
-
-// Se ejeccutara cuando nuestra pagina se recargue
-
-import { changeView } from './spa/route.js';
-
-const secInicio = document.getElementById('secinicio');
-const sectionOne = document.createElement('section1');
-const invitation = 'O bien ingresa con...';
-sectionOne.innerHTML = `${invitation} <br> <button class="googleButton">Google Login</button>`;
-secInicio.appendChild(sectionOne);
-// En este archivo se esta escuchando el camnio de la URL
-// Funcion para cambiar la url usando el evento hashchange
-
-const init = () => {
-  changeView(window.location.hash);
-  window.addEventListener('hashchange', () => changeView(window.location.hash));
-};
-window.addEventListener('load', init);
+buttonLogout.addEventListener('click', async (e) => {
+  logout();
+  console.log(e, 'no hay usuarios logueados');
+});
