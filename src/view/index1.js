2f3cc89e85f84f3d74a4fa7a3e9d8141a61c3527
giovanna-crypto <giovannalopez826@gmail.com>
2021-10-12 11:09:44 -1100

    practicando spa

--- src/view/index1.js
+++ src/view/index1.js
@@ -1,11 +1,11 @@
 /* aqui vamos a crear el objeto que reuna a todos los
  archivos de view para no exportarlas de Una en uno */
 
-import Home from './home.js';
-import Catalogo from './catalogo.js';
+import home from './home.js';
+import catalogo from './catalogo.js';
 
 const components = {
-  home: Home,
-  catalogo: Catalogo,
+  home,
+  catalogo,
 };
 export { components };
--- src/viewcontroller/index.js
+++ src/viewcontroller/index.js
@@ -1,4 +1,4 @@
-import { components } from '../view/index.js';
+import { components } from '../view/index1.js';
 
 const changeView = (route) => {
   const container = document.getElementById('container');
